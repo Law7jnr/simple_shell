afce4cebedf4c8e8c00763069899e6c7ad5445eb
Lawson-JR <140719079+Lawson-JR@users.noreply.github.com>
2023-11-08 18:35:03 +0100

    Create Unix-3.sh

---
+++ Unix-3.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Accessing command-line argument for command
+command=$1
+
+# Check if command exists in PATH
+if ! command -v "$command" > /dev/null; then
+  echo "Error: Command '$command' not found."
+  exit 1
+fi
+
+# Execute the command
+"$@"
